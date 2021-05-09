5748ab3ad04ec21283977ee3b50d08b662179a1a
Daniele <danielelai86@gmail.com>
2021-05-09 10:16:38 +0200

    Aggiunto footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,45 +1,67 @@
 <footer>
-    <div class="copyright"></div>
-    <div class="menu_footer">
-        <div class="nav_footer">
-            <h3>Pastificio</h3>
-            <nav>
-                <ul>
-                    <li><a href="#">Il Pastificio</a></li>
-                    <li><a href="#">Grano decorticato a pietra</a></li>
-                    <li><a href="#">Il Molise c'è</a></li>
-                    <li><a href="#">Filiera integrata</a></li>
-                    <li><a href="#">100 anni di pasta</a></li>
-                    <li><a href="#">Sartoria della pasta</a></li>
-                    <li><a href="#">Spaghetto Quadrato</a></li>
-                    <li><a href="#">Le Persone</a></li>
-                </ul>
-            </nav>
+    <div class="container">
+        <div class="copyright">
+            <img src={{ asset("images/marchio-sito-test.png") }} alt="Logo">
+            <div class="contacts">
+                <p>
+                Ragione sociale: La Molisana S.P.A.<br>
+                Sede legale: Contrada Colle Delle API, 100/A - 86100 - Campobasso (CB)<br>
+                Pec: lamolisana@pec.it<br>
+                Tel: +39 0874 4981<br>
+                Fax: +39 0874 629584<br>                  
+                info@lamolisana.it (per segnalazioni degli utenti)<br>
+                commerciale@lamolisana.it<br>
+                export@lamolisana.it<br>
+                numero verde:800818081<br>
+                telefono: 3801292455<br>
+                </p>
+            </div>
         </div>
-        <div class="nav_footer">
-            <h3>Collezione da chef</h3>
-            <nav>
-                <ul>
-                    <li><a href="#">Collezione da Chef </a></li>
-                    <li><a href="#">Grandi Cucine</a></li>
-                    <li><a href="#">Biologiche</a></li>
-                    <li><a href="#">Quadrate</a></li>
-                </ul>
-            </nav>
-        </div>
-        <div class="nav_footer">
-            <h3>Prodotti</h3>
-            <nav>
-                <ul>
-                    <li><a href="#">Le Classiche</a></li>
-                    <li><a href="#">Le Integrali</a></li>
-                    <li><a href="#">Le Speciali</a></li>
-                    <li><a href="#">Le Biologiche</a></li>
-                    <li><a href="#">Le Gluten-Free</a></li>
-                    <li><a href="#">Le Semole</a></li>
-                    <li><a href="#">Le Extra di Lusso</a></li>
-                </ul>
-            </nav>
+        <div class="menu_footer">
+            <div class="col-left">
+                <div class="nav_footer">
+                    <h3>Pastificio</h3>
+                    <nav>
+                        <ul>
+                            <li><a href="#">Il Pastificio</a></li>
+                            <li><a href="#">Grano decorticato a pietra</a></li>
+                            <li><a href="#">Il Molise c'è</a></li>
+                            <li><a href="#">Filiera integrata</a></li>
+                            <li><a href="#">100 anni di pasta</a></li>
+                            <li><a href="#">Sartoria della pasta</a></li>
+                            <li><a href="#">Spaghetto Quadrato</a></li>
+                            <li><a href="#">Le Persone</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="nav_footer">
+                    <h3>Prodotti</h3>
+                    <nav>
+                        <ul>
+                            <li><a href="#">Le Classiche</a></li>
+                            <li><a href="#">Le Integrali</a></li>
+                            <li><a href="#">Le Speciali</a></li>
+                            <li><a href="#">Le Biologiche</a></li>
+                            <li><a href="#">Le Gluten-Free</a></li>
+                            <li><a href="#">Le Semole</a></li>
+                            <li><a href="#">Le Extra di Lusso</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            <div class="col-right">
+                <div class="nav_footer">
+                    <h3>Collezione da chef</h3>
+                    <nav>
+                        <ul>
+                            <li><a href="#">Collezione da Chef </a></li>
+                            <li><a href="#">Grandi Cucine</a></li>
+                            <li><a href="#">Biologiche</a></li>
+                            <li><a href="#">Quadrate</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
         </div>
     </div>
 </footer>
